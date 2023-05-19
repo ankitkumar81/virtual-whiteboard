ea02cc24ecc68cf12b39fb61e3a5bdd0a58bbb02
Ankit Kumar <68067460+ankitkumar81@users.noreply.github.com>
2023-05-19 23:51:45 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,36 @@
+import torch
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(63, 1000),
+            torch.nn.BatchNorm1d(1000),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.2),
+
+            torch.nn.Linear(1000, 500),
+            torch.nn.BatchNorm1d(500),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.2),
+
+            torch.nn.Linear(500, 200),
+            torch.nn.BatchNorm1d(200),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.2),
+
+            torch.nn.Linear(200, 50),
+            torch.nn.ReLU(),
+
+            torch.nn.Linear(50, 3)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+def test():
+    model = Model()
+    noise = torch.randn((20, 63))
+    out = model.forward(noise)
+    print(out.shape)

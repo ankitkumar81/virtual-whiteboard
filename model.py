61e68f2f090ba41e8c279b55e000091b119d9229
Ankit Kumar <68067460+ankitkumar81@users.noreply.github.com>
2023-05-20 09:36:46 +0530

    model.py

--- model.py
+++ model.py
@@ -4,31 +4,30 @@ import torch.nn as nn
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.layers = torch.nn.Sequential(
-            torch.nn.Linear(63, 1000),
-            torch.nn.BatchNorm1d(1000),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(p=0.2),
+        self.layers = nn.Sequential(
+            nn.Linear(63, 1000),
+            nn.ReLU(),
+            nn.BatchNorm1d(1000),
+            nn.Dropout(p=0.5),
 
-            torch.nn.Linear(1000, 500),
-            torch.nn.BatchNorm1d(500),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(p=0.2),
+            nn.Linear(1000, 500),
+            nn.ReLU(),
+            nn.BatchNorm1d(500),
+            nn.Dropout(p=0.5),
 
-            torch.nn.Linear(500, 200),
-            torch.nn.BatchNorm1d(200),
-            torch.nn.ReLU(),
-            torch.nn.Dropout(p=0.2),
+            nn.Linear(500, 200),
+            nn.ReLU(),
+            nn.BatchNorm1d(200),
+            nn.Dropout(p=0.5),
 
-            torch.nn.Linear(200, 50),
-            torch.nn.ReLU(),
+            nn.Linear(200, 50),
+            nn.ReLU(),
 
-            torch.nn.Linear(50, 3)
+            nn.Linear(50, 3)
         )
 
     def forward(self, x):
         return self.layers(x)
-
 
 def test():
     model = Model()
